7ec8e9f061d6933bb4bc657fdfc26e26bb7ca016
MariaPizzeria <107207681+mariapizzeria@users.noreply.github.com>
2026-05-20 16:58:29 +0500

    тест очереди

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
-	"os"
 	"rabbit/internal/order"
+	"rabbit/queue-config/consumer"
+	"rabbit/queue-config/producer"
 )
 
 var (
-	email  = "test@2.com"
-	price  = 100
-	amount = 3
+	email     = "test@2.com"
+	price     = 100
+	amount    = 3
+	queueName = "test"
 )
 
 func main() {
 	NewOrder := order.NewOrder(email, price, amount)
+	newProducer, err := producer.NewProducer(queueName)
+	if err != nil {
+		panic(err)
+	}
+	defer newProducer.Close()
 
+	newConsumer, err := consumer.NewConsumer()
+	if err != nil {
+		panic(err)
+	}
+	defer newConsumer.Close()
+	go func() {
+		err = newConsumer.ProcessMessage(queueName, NewOrder)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	err = newProducer.SendMessage(NewOrder)
+	if err != nil {
+		panic(err)
+	}
 }
--- queue-config/consumer/consumer.go
+++ queue-config/consumer/consumer.go
@@ -1,1 +1,62 @@
 package consumer
+
+import (
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+type Consumer struct {
+	conn    *amqp.Connection
+	channel *amqp.Channel
+}
+
+func NewConsumer() (*Consumer, error) {
+	consumer, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		return nil, err
+	}
+	ch, err := consumer.Channel()
+	if err != nil {
+		return nil, err
+	}
+	return &Consumer{conn: consumer, channel: ch}, nil
+}
+
+func (c *Consumer) ProcessMessage(queueName string, T any) error {
+	q, err := c.channel.QueueDeclare(queueName, false, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+	msgs, err := c.channel.Consume(q.Name, "", true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+	log.Printf("Consumer got %d messages", len(msgs))
+	forever := make(chan bool)
+	go func() {
+		for d := range msgs {
+			err = json.Unmarshal(d.Body, T)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			log.Printf("Consumer got message: %s", d.Body)
+			processAny(T)
+		}
+	}()
+	<-forever
+	return nil
+}
+
+func processAny(T any) {
+	log.Printf("Processing email: %d", T) // simulate
+	time.Sleep(10 * time.Second)
+}
+
+func (c *Consumer) Close() {
+	c.conn.Close()
+	c.channel.Close()
+}
